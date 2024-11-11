cf2749e0e9165a1095584a03cb81a403cb28e6e6
LuxScintilla <catherina.ea@gmail.com>
2024-11-11 12:07:45 +0000

    add populated by api homepage

--- main.js
+++ main.js
@@ -36,7 +36,6 @@ async function createFeatured() {
   const data = await getApiData("movie/now_playing");
   const number = generateRandomNumber();
   const featuredMovie = data[number];
-  console.log(data);
 
   const featuredMovieDIV = document.createElement("div");
   featuredMovieDIV.classList.add("featured-movie");
@@ -66,14 +65,69 @@ async function createFeatured() {
   featuredInfoDIV.appendChild(btn);
   featuredMovieDIV.appendChild(featuredInfoDIV);
 
-  // BROKEN HERE CONTINUE HERE BROKEN HERE CONTINUE HERE!!!!!!!!!!!!!!!!
+  mainContent.childNodes.forEach((node) => {
+    if (node.classList.contains("top-movies-container")) {
+      mainContent.insertBefore(featuredMovieDIV, node);
+    }
+  });
+}
 
-  // mainContent.appendChild(featuredMovieDIV);
-  console.log(mainContent.childNodes[0].classList.contains("footer"));
-  // mainContent.insertBefore(
-  //   featuredMovieDIV,
-  //   mainContent.childNodes.classList.contains("footer")
-  // );
+// CREATE TOP 5 MOVIES ON HOME PAGE ---------------------------------
+
+async function createTop5Movies() {
+  const data = await getApiData("movie/popular");
+  console.log(data);
+
+  const topMoviesDIV = document.createElement("div");
+  topMoviesDIV.classList.add("top-movies-container");
+
+  const sectionText = document.createElement("h3");
+  sectionText.classList.add("top-movies-section-text");
+  sectionText.textContent = "Top 5 Movies";
+
+  topMoviesDIV.appendChild(sectionText);
+
+  for (let i = 0; i <= 4; i++) {
+    const movieDIV = document.createElement("div");
+    movieDIV.classList.add("top-movie");
+    movieDIV.setAttribute("data-id", data[i].id);
+
+    const movieIMG = document.createElement("img");
+    movieIMG.classList.add("top-movie-img");
+    movieIMG.setAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/w500${data[i].poster_path}`
+    );
+    movieIMG.setAttribute("alt", "movie poster");
+
+    const movieTextBoxDIV = document.createElement("div");
+    movieTextBoxDIV.classList.add("top-text-box");
+
+    const movieTitle = document.createElement("p");
+    movieTitle.classList.add("top-title");
+    movieTitle.textContent = data[i].title;
+
+    const movieDescription = document.createElement("p");
+    movieDescription.classList.add("top-description");
+    movieDescription.textContent = `${data[i].overview.substring(0, 75)} ...`;
+
+    const movieBtn = document.createElement("button");
+    movieBtn.classList.add("top-btn");
+    movieBtn.textContent = "More Info";
+
+    movieTextBoxDIV.appendChild(movieTitle);
+    movieTextBoxDIV.appendChild(movieDescription);
+    movieTextBoxDIV.appendChild(movieBtn);
+    movieDIV.appendChild(movieIMG);
+    movieDIV.appendChild(movieTextBoxDIV);
+    topMoviesDIV.appendChild(movieDIV);
+
+    mainContent.childNodes.forEach((node) => {
+      if (node.classList.contains("footer")) {
+        mainContent.insertBefore(topMoviesDIV, node);
+      }
+    });
+  }
 }
 
 // CREATE FOOTER AT BOTTOM OF THE PAGE ------------------------------
@@ -105,10 +159,15 @@ function createFooter() {
     "https://developer.themoviedb.org/docs/getting-started"
   );
 
+  const apiIMG = document.createElement("img");
+  apiIMG.setAttribute("src", "./img/blue_long.svg");
+  apiIMG.setAttribute("alt", "the movie database api logo");
+
   inspirationPara.appendChild(linkDribbble);
   apiPara.appendChild(linkAPI);
   footerDIV.appendChild(inspirationPara);
   footerDIV.appendChild(apiPara);
+  footerDIV.appendChild(apiIMG);
   mainContent.appendChild(footerDIV);
 }
 
@@ -118,6 +177,7 @@ function createHome() {
   while (mainContent.firstChild) {
     mainContent.removeChild(mainContent.firstChild);
   }
+  createTop5Movies();
   createFeatured();
   createFooter();
 }
